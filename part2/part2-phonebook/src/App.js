d2e6f447abab732fb97576c22ada5870e1e1e5f6
mialav <mia.lavanti@wolt.com>
2023-04-09 23:05:14 +0300

    exercise 2.11

--- part2/part2-phonebook/src/App.js
+++ part2/part2-phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Person = ({ person }) => {
   return (
@@ -39,12 +40,16 @@ const NameFilter = ({ filterValue, handleFilter }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterValue, setFilterValue] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
